9a5614f0f0c3be436747d9040ef0ec508f7033a5
jhenry77 <108282181+jhenry77@users.noreply.github.com>
2024-02-28 20:38:32 -0500

    got address working

--- app/loginPage/page.tsx
+++ app/loginPage/page.tsx
@@ -2,21 +2,41 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
 
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, Placeholder } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from '../../src/aws-exports';
 Amplify.configure(awsExports);
+const signUpFields = {
+  signUp: {
+    email: {
+      order:1
+    },
+    password: {
+      order: 2
+    },
+    confirm_password: {
+      order: 3
+      
+    },
+    address: {
+      order: 4,
+      label: 'Address',
+      Placeholder: 'Enter your Address'
+    },
+    name:{
+      order:5
+    },
+    family_name:{
+      order:6
+    }
+  },
+}
 
 export default function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          {/* <h1>Hello {user.username}</h1> */}
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
+    <Authenticator formFields={signUpFields}>
+      <h1>hello</h1>
     </Authenticator>
   );
 }
