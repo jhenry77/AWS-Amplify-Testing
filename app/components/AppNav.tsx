8743a4b05cd6fdae6f64a79ee7a4e2942e20f7d8
jasonsenf <jmaxsenf@gmail.com>
2024-03-07 12:37:35 -0500

    comment

--- app/components/AppNav.tsx
+++ app/components/AppNav.tsx
@@ -14,28 +14,3 @@ export default function Navbar() {
     </nav>
 };
 
-/*
-export const Navbar:React.FC = () => {
-    return <header className="navbar bg-base-100">
-        <div className="navbar-start">
-            <div className="px-3">
-                    <Link href="/home">
-                        Home
-                    </Link>
-            </div>
-            <div className="px-3">
-                <Link href="/">
-                    About
-                </Link>
-            </div>
-            <div className="px-3">
-                <Link href="/loginPage">
-                    Login/Signup
-                </Link>
-            </div>
-        </div>
-    </header>;
-};
-
-export default Navbar
-*/
