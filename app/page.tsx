a1cc9922679e3e8a50b85db588671fc81f3a09f8
jasonsenf <jmaxsenf@gmail.com>
2024-03-13 00:20:06 -0400

    styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import styles from "./components/styles/about.module.css";
 
 type AboutData = {
   teamNumber: string
@@ -16,32 +17,33 @@ export default async function Home() {
   console.log(data);
   return (
     <main>
-      <div className="max-w-4xl w-full text-center">
-        <h1 className="text-4xl font-bold mb-4">About Us</h1>
+        <h1 className={styles['title']}>About Us</h1>
         <Image
           src="/teamlogo.jpg"
           alt="Team Photo"
-          width={640}
-          height={360}
+          width={400}
+          height={400}
           className="mx-auto"
         />
-        <p className="text-lg mt-6">
+        <br/>
+        <p className={styles['text']}>
           Welcome to <strong>Team 3&apos;s Website</strong>! We&apos;re dedicated to providing the best experience for our users. <br />
           <strong>Our mission is to innovate and inspire.</strong>
         </p>
-        <p className="text-lg mt-4">
+        <p className={styles['text']}>
         <strong>Team Members:  </strong><br />
           Connor Love <br /> 
           Rinzo Martinelli <br /> 
           Jason Senf<br /> 
           Jackson Henry <br/>
           <br />
-          With a focus on quality and community, we strive to bring you the latest in our field. Our team is made up of passionate professionals committed to excellence in everything we do.
+          With a focus on quality and community, we strive to bring you the latest in our field.<br/> Our team is made up of passionate professionals committed to excellence in everything we do.
           <br />
+          <br/>
         </p>
         {data ? (
           <>
-            <h1 className="text-4xl  mb-4">teamNumber: {data.success[0].teamNumber}<br></br>VersionNum: {data.success[0].VersionNum}<br></br>SprintDate: {data.success[0].SprintDate}<br></br>Productname: {data.success[0].ProductName}<br></br>Product Description: {data.success[0].ProductDescription} </h1>
+            <h1 className={styles['text']}>Crlt Alt Elt: {data.success[0].teamNumber}<br></br>Verison: {data.success[0].VersionNum}<br></br>Date: {data.success[0].SprintDate}<br></br>Product: {data.success[0].ProductName}<br></br>Description: {data.success[0].ProductDescription} </h1>
             {/* Other HTML elements using data properties */}
             {/* <p className="text-lg mt-6">{data.description}</p> */}
             {/* ... */}
@@ -49,7 +51,6 @@ export default async function Home() {
         ) : (
           <h1>Loading...</h1>
         )}
-      </div>
     </main>
   );
 }
