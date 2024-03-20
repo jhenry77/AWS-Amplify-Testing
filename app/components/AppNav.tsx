7f2b1a5f5181738361d5b7b169285b78e54e7cf7
calove <calove@clemson.edu>
2024-03-20 17:52:27 -0400

    Right Side NavBar button FIX

--- app/components/AppNav.tsx
+++ app/components/AppNav.tsx
@@ -3,7 +3,8 @@ import Link from "next/link"
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { signOut } from "aws-amplify/auth";
 import styles from "./styles/AppNav.module.css";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import HamburgerMenu from "./HamburgerMenu";
 
 type NavbarProps = {
     onLeftMenuToggle: () => void; // Function to toggle the left menu
@@ -15,8 +16,12 @@ export default function Navbar() {
     // const { user, signOut } = useAuthenticator((context) => [context.user]);
     // const { authStatus } = useAuthenticator((context) => [context.authStatus]);
     const [showDropdown, setShowDropdown] = useState(false);
+    const [showLeftMenu, setShowLeftMenu] = useState(true);
+    const [showRightMenu, setShowRightMenu] = useState(false);
 
     const toggleDropdown = () => setShowDropdown(!showDropdown);
+    const toggleLeftMenu = () => setShowLeftMenu(!showLeftMenu);
+    const toggleRightMenu = () => setShowRightMenu(!showRightMenu);
 
     if (authStatus !== 'authenticated') {
         return null;
@@ -34,7 +39,7 @@ export default function Navbar() {
         <nav className={styles['navbar']}>
             <div className="left-content">
                 {/* Button that when clicked, will toggle the state of the left hamburger menu */}
-                <button className="hamburger-menu-button" aria-label="Open navigation menu">☰</button>
+                <button onClick={toggleLeftMenu} className="hamburger-menu-button" aria-label="Open navigation menu">☰</button>
                 <Link href="/home" className="nav-link">Home</Link>
                 <Link href="/" className="nav-link">About</Link>
                 <Link href="/" className="nav-link">Sponsors</Link>
@@ -45,9 +50,14 @@ export default function Navbar() {
             <div className="right-content">
                 {authStatus === 'authenticated' && user ? (
                     <>
-                        <span className="user-info" onClick={toggleDropdown}>
-                            {displayName} ▼  <button /*onClick={onRightMenuToggle}*/ className="settings-menu-button" aria-label="Open settings">⚙️{/* Settings icon */}
-                </button>
+                        <span className="user-info">
+                            {displayName} 
+                            <button onClick={toggleDropdown} className="dropdown-menu-button" aria-label="Open dropdown">
+                                ▼{/* Dropdown menu icon */}
+                            </button>
+                            <button onClick={toggleRightMenu} className="settings-menu-button" aria-label="Open settings">
+                                ⚙️{/* Settings icon */}
+                            </button>
                         </span>
                         {showDropdown && (
                             <div className={`dropdown-menu ${showDropdown ? 'show-dropdown' : ''}`} role="menu">
