1d8a1e6271aedaa9592cdc3635e6940966abcfa0
Connor Love <123567579+ConnorLove0@users.noreply.github.com>
2024-04-10 20:48:29 -0400

    Update AppNav.tsx

--- app/components/AppNav.tsx
+++ app/components/AppNav.tsx
@@ -1,206 +1,167 @@
-/* AppNav.module.css */
+// "use client";
+// import Link from "next/link"
+// import { useAuthenticator } from '@aws-amplify/ui-react';
+// import { signOut } from "aws-amplify/auth";
+// import styles from "./styles/AppNav.module.css";
+// import DropdownMenu from "./DropDown";
 
-.navbar {
-    position: fixed;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    /* This ensures items are spaced between left and right */
-    font-size: 1rem;
-    padding: 0.5rem 1rem;
-    /* color: #ecf0f1; */
-    /* text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.7); */
-    background-color: #2c3e50;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    width: 100%;
-    margin-bottom: 10%;
-    z-index: 1500;
+// export default function Navbar() {
+//     const { user, signOut } = useAuthenticator((context) => [context.user]);
+//     const {authStatus} = useAuthenticator((context) => [context.authStatus]);
+
+//     if (authStatus !== 'authenticated'){
+//         return null;
+//     }
+
+//     return <nav className={styles['navbar']}>
+
+//             <Link href="/home">Home</Link>
+//             <Link href="/">About</Link>
+//             <Link href="/">Sponsors</Link>
+//             <Link href="/">Catalogs</Link>
+//             <Link href="/">Reports</Link>
+//                 <>
+//                 {authStatus !== 'authenticated' ? <Link href="/login">Login</Link> : <button className = "justify-end" onClick={signOut}>Sign Out</button>}
+//                 </>
+//             <DropdownMenu/>
+//     </nav>
+// };
+// AppNav.tsx
+"use client";
+import Link from "next/link"
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { signOut } from "aws-amplify/auth";
+import styles from "./styles/AppNav.module.css";
+import HamburgerMenu from "./HamburgerMenu";
+import React, { useState, useEffect } from 'react';
+import { fetchAuthSession } from "aws-amplify/auth";
+import { useRouter } from "next/navigation";
+import CartUi from "./CartUi";
+
+
+export default function Navbar() {
+    const router = useRouter();
+
+    const { user, signOut, authStatus } = useAuthenticator((context) => [context.user, context.signOut, context.authStatus]);
+    // const { user, signOut } = useAuthenticator((context) => [context.user]);
+    // const { authStatus } = useAuthenticator((context) => [context.authStatus]);
+
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [showPointsDropdown, setShowPointsDropdown] = useState(false);
+    const [showLeftMenu, setShowLeftMenu] = useState(true); /* change to true */
+    const [showRightMenu, setShowRightMenu] = useState(false);
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    const togglePointsDropdown = () => {
+        setShowPointsDropdown(!showPointsDropdown);
+    };
+
+    const toggleLeftMenu = () => {
+        setShowLeftMenu(!showLeftMenu);
+    };
+
+    const toggleRightMenu = () => {
+        setShowRightMenu(!showRightMenu);
+    };
+
+    const [groups, setGroups] = useState(undefined);
+    const [userName, setUserName] = useState(String);
+
+    useEffect(() => {
+        fetchAuthSession({ forceRefresh: true })
+            .then(({ tokens }) => {
+                const idToken = tokens?.idToken as any;
+                console.log(idToken);
+                const userGroups = idToken.payload['cognito:groups'];
+                const username = idToken.payload["name"] + " " + idToken.payload["family_name"];
+                setGroups(userGroups ? userGroups[0] : "No active Groups");
+                setUserName(username);
+                console.log(username);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+    useEffect(() => {
+        if (userName === undefined) {
+            router.refresh();
+        }
+    }, [router, userName]);
+
+    // Log the current user object to the console
+    return (
+        <nav className={styles['navbar']}>
+            <div className="navbar-left-content">
+                {/* Button that when clicked, will toggle the state of the left hamburger menu */}
+                <button onClick={toggleLeftMenu} className="hamburger-menu-button" aria-label="Open navigation menu">☰</button>
+                {/* <HamburgerMenu showLeftMenu={showLeftMenu} /> */}
+
+                <Link href="/home" className="nav-link">Home</Link>
+                <Link href="/" className="nav-link">About</Link>
+                <Link href="/sponsorApplication" className="nav-link">Sponsor Applications</Link>
+                <Link href="/testCatalog" className="nav-link">Catalogs</Link>
+                <Link href="/" className="nav-link">Reports</Link>
+            </div>
+
+            <div className={styles["navbar-right-content"]}>
+                {authStatus === 'authenticated' && user ? (
+                    <>
+                        <span className="points-info">
+                            {"0 points"}
+                            {/* {userName} */}
+                            <button onClick={togglePointsDropdown} className="points-dropdown-menu-button" aria-label="Open dropdown">
+                                ▼{/* Dropdown menu icon */}
+                            </button>
+                        </span>
+                        <CartUi/>
+                        <span className="user-info">
+                            {userName}
+                            <button onClick={toggleDropdown} className="dropdown-menu-button" aria-label="Open dropdown">
+                                ▼{/* Dropdown menu icon */}
+                            </button>
+                        </span>
+
+                        <button onClick={toggleRightMenu} className="settings-menu-button" aria-label="Open settings">
+                            ⚙️{/* Settings icon */}
+                        </button>
+                        {/* <HamburgerMenu showRightMenu={showRightMenu} /> */}
+
+                        {showPointsDropdown && (
+                            <div className={`${styles['points-dropdown-menu']} ${showPointsDropdown ? styles['show-dropdown'] : ''}`} role="menu">
+                                <Link href="/points">My Points</Link>
+                            </div>
+                        )}
+
+                        {showDropdown && (
+                            <div className={`${styles['dropdown-menu']} ${showDropdown ? styles['show-dropdown'] : ''}`} role="menu">
+                                <Link href="/profile">Profile</Link>
+                                <button className="sign-out-button" onClick={signOut}>Sign Out</button>
+                            </div>
+                        )}
+                    </>
+                ) : (
+                    <Link href="/login">Sign In</Link>
+                )}
+            </div>
+            
+
+            {/* Integrating HamburgerMenu component here */}
+            <HamburgerMenu
+                showLeftMenu={showLeftMenu}
+                showRightMenu={showRightMenu}
+                onCloseLeftMenu={() => setShowLeftMenu(false)} // Close left menu
+                onCloseRightMenu={() => setShowRightMenu(false)} // Close right menu
+            />
+        </nav>
+    );
 }
 
-.settings-menu-button {
-    margin-left: auto;
-    /* This will push the settings button to the far right */
-}
-
-/* Navbar Items */
-.navbar a,
-.navbar button {
-    color: inherit;
-    text-decoration: none;
-    padding: 0.25rem 0.5rem;
-    transition: background-color 0.3s ease;
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.7);
-    -webkit-transition: background-color 0.3s ease;
-    -moz-transition: background-color 0.3s ease;
-    -ms-transition: background-color 0.3s ease;
-    -o-transition: background-color 0.3s ease;
-}
-
-/* Hover effect for navigation links */
-.navbar a:hover,
-.navbar button:hover {
-    background-color: #34495e;
-}
-
-.navbar-left-content {
-    padding: 0.25rem 0.5rem;
-}
-
-
-.navbar-right-content {
-    position: relative;
-    display: flex;
-    /* Use Flexbox for alignment */
-    align-items: center;
-    /* Vertically center the content */
-    /* max-width: fit-content; */
-    /* Or set a specific width if needed */
-    justify-content: space-between;
-    text-align: right;
-    /* Push content to the end (right side) */
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.7);
-}
-
-.username-dropdown {
-    background: none;
-    border: none;
-    cursor: pointer;
-    font-size: 1rem;
-    /* Match your design */
-}
-
-.user-info {
-    position: relative;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    text-align: center;
-}
-
-.points-info {
-    position: relative;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    text-align: center;
-}
-
-.dropdown-menu {
-    display: none;
-    /* Hide dropdown initially */
-    position: absolute;
-    top: 100%;
-    /* Position the dropdown directly below the navbar */
-    right: 0;
-    /* Align the dropdown to the right of the navbar */
-    background-color: white;
-    /* Set a background color */
-    border: 1px solid #ccc;
-    /* Optional: Add a border */
-    min-width: 60px;
-    text-align: center;
-    /* Minimum width of the dropdown */
-    z-index: 100;
-    /* Ensure it overlays other content */
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
-
-.points-dropdown-menu {
-    display: none;
-    /* Hide dropdown initially */
-    position: absolute;
-    top: 100%;
-    /* Position the dropdown directly below the navbar */
-    left: 0;
-    /* Align the dropdown to the right of the navbar */
-    background-color: white;
-    /* Set a background color */
-    color: black;
-    /* border: 1px solid #ccc; */
-    /* Optional: Add a border */
-    width: auto;
-    /* Minimum width of the dropdown */
-    z-index: 100;
-    /* Ensure it overlays other content */
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
-
-.show-dropdown {
-    display: block;
-    /* Show the dropdown */
-}
-
-/* Add :not(.show-dropdown) to hide the dropdown when it's not supposed to show */
-.navbar .navbar-right-content .dropdown-menu:not(.show-dropdown) {
-    display: none;
-}
-
-button.sign-out-button {
-    text-align: center;
-}
-
-.dropdown-menu a,
-.dropdown-menu button, 
-.dropdown-menu-button, 
-.points-dropdown-menu a, 
-.points-dropdown-menu button {
-    display: block;
-    padding: 10px;
-    text-align: center;
-    /* Align text to the left */
-    background: none;
-    border: none;
-    text-decoration: none;
-    color: black;
-    /* Text color */
-    cursor: pointer;
-}
-
-.dropdown-menu-button,
-.dropdown-menu button {
-    text-align: center;
-}
-
-
-.points-dropdown-menu button:hover,
-.points-dropdown-menu a:hover {
-    background-color: #f5f5f5;
-    /* Slight highlight on hover */
-}
-
-.dropdown-menu button:hover,
-.dropdown-menu a:hover {
-    background-color: #f5f5f5;
-    /* Slight highlight on hover */
-}
-
-.settings-menu-button {
-    cursor: pointer;
-}
-
-.menu-title {
-    font-size: 20px;
-    /* Make sure this is large enough */
-    font-weight: bold;
-    color: #2C3E50;
-    /* Ensure contrast with the background */
-    padding: 20px;
-    border-bottom: 1px solid #ccc;
-    /* Optional: adds a separator */
-    margin-bottom: 20px;
-}
-
-/* .right-close-button {
-    left: auto;
-    padding: 0rem 0rem;
-} */
-
-/* Ensure this media query encompasses the screen sizes you're targeting */
-@media (max-width: 780) {
-    .navbar {
-        /* Use your actual navbar class */
-        z-index: 1500;
-        /* Lower z-index than the menu */
-        flex-shrink: 1;
-    }
-}
+// // Main content component
+// const MainContent = ({ showLeftMenu }) => (
+//     <div className={`main-content ${showLeftMenu ? 'main-content-with-menu' : ''}`}>
+//       {/* Your main content */}
+//     </div>
+//   );
