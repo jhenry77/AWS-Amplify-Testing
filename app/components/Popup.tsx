68431896c387d5c7a67d5bb57298acc3329505c4
Connor Love <123567579+ConnorLove0@users.noreply.github.com>
2024-04-12 11:08:52 -0400

    Update Popup.tsx

--- app/components/Popup.tsx
+++ app/components/Popup.tsx
@@ -2,14 +2,21 @@
 import React from 'react';
 import styles from './styles/Popup.module.css'; // Update the path to your actual CSS file
 
-const Popup = ({popupTitle, onClose, children }) => {
+// Define an interface for the component props
+interface PopupProps {
+  popupTitle: string;  // Define the type of popupTitle as string
+  onClose: () => void; // Define the type of onClose as a function returning void
+  children?: React.ReactNode;  // Optionally include children of type React.ReactNode
+}
+
+const Popup: React.FC<PopupProps> = ({ popupTitle, onClose, children }) => {
   return (
     <div className={styles.popupBackground}>
       <div className={styles.popup}>
         <h2 className={styles.popupTitle}>{popupTitle}</h2>
         {children}
         <div className={styles.popupButtons}>
-            <button type="button" className="popup-exit-button" onClick={onClose}>Exit</button>
+            <button type="button" className={styles.popupExitButton} onClick={onClose}>Exit</button>
         </div>
       </div>
     </div>
