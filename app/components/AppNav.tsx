6695790d69b6afb1ced6cfdc39da386cd9ce37db
Connor Love <123567579+ConnorLove0@users.noreply.github.com>
2024-04-18 12:47:57 -0400

    Update AppNav.tsx

--- app/components/AppNav.tsx
+++ app/components/AppNav.tsx
@@ -1,31 +1,3 @@
-// "use client";
-// import Link from "next/link"
-// import { useAuthenticator } from '@aws-amplify/ui-react';
-// import { signOut } from "aws-amplify/auth";
-// import styles from "./styles/AppNav.module.css";
-// import DropdownMenu from "./DropDown";
-
-// export default function Navbar() {
-//     const { user, signOut } = useAuthenticator((context) => [context.user]);
-//     const {authStatus} = useAuthenticator((context) => [context.authStatus]);
-
-//     if (authStatus !== 'authenticated'){
-//         return null;
-//     }
-
-//     return <nav className={styles['navbar']}>
-
-//             <Link href="/home">Home</Link>
-//             <Link href="/">About</Link>
-//             <Link href="/">Sponsors</Link>
-//             <Link href="/">Catalogs</Link>
-//             <Link href="/">Reports</Link>
-//                 <>
-//                 {authStatus !== 'authenticated' ? <Link href="/login">Login</Link> : <button className = "justify-end" onClick={signOut}>Sign Out</button>}
-//                 </>
-//             <DropdownMenu/>
-//     </nav>
-// };
 // AppNav.tsx
 "use client";
 import Link from "next/link"
